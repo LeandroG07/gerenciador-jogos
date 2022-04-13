d44121bf105917704484765d928040ad119144bb
Leandro Galli <leandro.galli@invillia.com>
2022-04-13 16:27:08 -0300

    compose executando scripts em banco de dados apos inicializacao do container via DOCKER COMPOSE

--- scripts/deploy.sql
+++ scripts/deploy.sql
@@ -1,6 +1,6 @@
-:r ./000_Create_Database_jogodb.sql
-:r ./001_Create_Table_Amigo.sql
-:r ./002_Create_Table_Jogo.sql
-:r ./003_Create_Table_Login.sql
-:r ./004_Insert_Table_Login.sql
-:r ./005_Create_Table_Emprestimo.sql
+:r ./scripts/000_Create_Database_jogodb.sql
+:r ./scripts/001_Create_Table_Amigo.sql
+:r ./scripts/002_Create_Table_Jogo.sql
+:r ./scripts/003_Create_Table_Login.sql
+:r ./scripts/004_Insert_Table_Login.sql
+:r ./scripts/005_Create_Table_Emprestimo.sql
