618eacc69a73bdca934bcb5e049f3343cc6822c6
Leandro Galli <leandro.galli@invillia.com>
2022-04-13 12:27:31 -0300

    criacao scripts deploy inicial sql

---
+++ scripts/deploy.sql
@@ -0,0 +1,6 @@
+:r ./000_Create_Database_jogodb.sql
+:r ./001_Create_Table_Amigo.sql
+:r ./002_Create_Table_Jogo.sql
+:r ./003_Create_Table_Login.sql
+:r ./004_Insert_Table_Login.sql
+:r ./005_Create_Table_Emprestimo.sql
